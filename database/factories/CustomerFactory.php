2e5aa86793f443e27c0500e2fb7e56d1a05b2354
Nikita Niatin <yesnik@yandex.ru>
2022-12-30 16:53:49 +0500

    Add CustomerSeeder

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,8 +16,17 @@ class CustomerFactory extends Factory
      */
     public function definition()
     {
+        $type = $this->faker->randomElement(['individual', 'business']);
+        $name = ($type === 'individual') ? $this->faker->name() : $this->faker->company();
+
         return [
-            //
+            'name' => $name,
+            'mobile' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email(),
+            'city' => $this->faker->city(),
+            'address' => $this->faker->address(),
+            'postal_code' => $this->faker->postcode(),
+            'type' => $type,
         ];
     }
 }
